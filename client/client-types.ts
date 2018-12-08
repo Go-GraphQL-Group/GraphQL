b89b868f14d30c7288524ba7226247e35a0eb926
Ernie1 <870949712@qq.com>
2018-12-08 14:14:48 +0800

    provide the reference

---
+++ client/client-types.ts
@@ -0,0 +1,11 @@
+/** The class of vehicle in the Star Wars universe. */
+export enum VehicleClass {
+  Wheeled = "WHEELED",
+  Repulsocraft = "REPULSOCRAFT"
+}
+/** The genders of people in the Star Wars universe. */
+export enum Gender {
+  Na = "NA",
+  Male = "MALE",
+  Female = "FEMALE"
+}
